708391c0bac4587c73a87f2654ef2818c6a0daa2
vietcuongk99 <daoquocthai8@gmail.com>
2021-06-29 13:17:38 +0700

    add platform slider

--- js/index.js
+++ js/index.js
@@ -1,21 +1,84 @@
-let collapseDropdown = document.getElementById('collapseDropdown'),
-    btnToggleDropDown = document.getElementById('btnToggleDropDown'),
-    secondNav = document.getElementById('secondNav'),
-    sticky = secondNav.offsetTop;
+let collapseDropdown = $('#collapseDropdown'),
+    btnToggleDropDown = $('#btnToggleDropDown'),
+    secondNav = $('#secondNav'),
+    sticky = secondNav.offset().top;
+
+$(document).ready(function() {
+    initEvent();
+});
 
 function initEvent() {
-    window.addEventListener("scroll", onScroll);
-    btnToggleDropDown.addEventListener('click', () => {
-        collapseDropdown.classList.toggle('d-none');
+    $(window).on("scroll", onScroll);
+    btnToggleDropDown.on('click', () => {
+        collapseDropdown.toggleClass('d-none');
     });
 }
 
 function onScroll() {
-    if (window.pageYOffset >= sticky) {
-        secondNav.classList.add("sticky-top--custom");
+    if ($(window).scrollTop() > sticky) {
+        secondNav.addClass("sticky-top--custom");
     } else {
-        secondNav.classList.remove("sticky-top--custom");
+        secondNav.removeClass("sticky-top--custom");
     }
 }
 
-initEvent();
+
+$('#platformSlider').slick({
+    dots: false,
+    arrow: true,
+    infinite: true,
+    autoplay: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+
+        {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        // {
+        //     breakpoint: 480,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1,
+        //         infinite: true,
+        //     }
+        // }
+    ]
+});
