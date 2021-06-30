9f11097b6afd016a60476c9b07acffcda8f0459b
vietcuongk99 <daoquocthai8@gmail.com>
2021-06-30 23:52:08 +0700

    add new card and slide

--- js/index.js
+++ js/index.js
@@ -12,6 +12,14 @@ function initEvent() {
     btnToggleDropDown.on('click', () => {
         collapseDropdown.toggleClass('d-none');
     });
+
+    $('.card__content--teacher').hover((event) => {
+        let eventTarget = $(event.target);
+        eventTarget.find('.card__sociallink--teacher').removeClass('d-none');
+    }, (event) => {
+        let eventTarget = $(event.target);
+        eventTarget.find('.card__sociallink--teacher').addClass('d-none');
+    })
 }
 
 function onScroll() {
@@ -72,8 +80,61 @@ $('#optionSlider').slick({
         },
     ]
 });
-
-// $('#coursesSlider').slick({
-//     prevArrow: $('#prevArrow'),
-//     nextArrow: $('#nextArrow'),
-// });
+$('#courseSlider').slick({
+    prevArrow: $('#prevArrow'),
+    nextArrow: $('#nextArrow'),
+    dots: false,
+    arrow: false,
+    infinite: true,
+    autoplay: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+    ],
+});
+$('#reviewContent').slick({
+    dots: true,
+    infinite: true,
+    arrow: false,
+    prevArrow: null,
+    nextArrow: null,
+    autoplay: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                arrow: false,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrow: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+    ],
+});
