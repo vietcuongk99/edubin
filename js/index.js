bf21b4a0f5804f63f04b3cd8a845c92b7cc4b5ff
vietcuongk99 <daoquocthai8@gmail.com>
2021-07-01 22:59:11 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-// let course = {
-
-//     "id": 1,
-//     "name": "30 Days Weight Loss Yoga & Fitness Course",
-//     "categories": "Fitness",
-//     "price": 18,
-//     "level": "Intermediate",
-//     "duration": "5h",
-//     "total_enrolled": 12,
-//     "teacher": "Mark Alen",
-//     "rate": 5,
-//     "rate_quantity": 3,
-//     "image": "https://i.imgur.com/88gNx6K.jpeg"
-// };
-
 let collapseDropdown = $('#collapseDropdown'),
     btnToggleDropDown = $('#btnToggleDropDown'),
     mainNav = $('#mainNav'),
@@ -20,8 +5,9 @@ let collapseDropdown = $('#collapseDropdown'),
     sticky = $('#topBar').offset().top;
 
 $(document).ready(function() {
-    initEvent();
+    addSlider();
     getCourseData();
+    initEvent();
 });
 
 function initEvent() {
@@ -29,18 +15,16 @@ function initEvent() {
     btnToggleDropDown.on('click', () => {
         collapseDropdown.toggleClass('d-none');
     });
-
     btnMoveToTop.on("click", moveToTop);
-
     $('.card__content--teacher>div').hover((event) => {
-        let eventTarget = $(event.target);
-        eventTarget.find('.card__sociallink--teacher').toggleClass("d-none");
+        toggleTeacherLink($(event.target));
     }, (event) => {
-        let eventTarget = $(event.target);
-        eventTarget.find('.card__sociallink--teacher').toggleClass("d-none");
+        toggleTeacherLink($(event.target));
     });
+}
 
-
+function toggleTeacherLink(target) {
+    target.find('.card__sociallink--teacher').toggleClass("d-none");
 }
 
 function onScroll() {
@@ -114,147 +98,149 @@ function loadCourseData(data) {
     </div>
 </div>`;
         $('#courseSlider').append(card);
-    }
+    };
+    $('#courseSlider').slick({
+        prevArrow: $('#prevArrow'),
+        nextArrow: $('#nextArrow'),
+        dots: false,
+        arrow: false,
+        infinite: true,
+        autoplay: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+        ],
+    });
 }
 
-$('#optionSlider').slick({
-    dots: false,
-    arrow: true,
-    infinite: true,
-    autoplay: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [{
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 1000,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 800,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 420,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-    ]
-});
-$('#courseSlider').slick({
-    prevArrow: $('#prevArrow'),
-    nextArrow: $('#nextArrow'),
-    dots: false,
-    arrow: false,
-    infinite: true,
-    autoplay: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [{
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-    ],
-});
-$('#reviewContent').slick({
-    dots: true,
-    infinite: true,
-    arrow: false,
-    prevArrow: null,
-    nextArrow: null,
-    autoplay: false,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    responsive: [{
-            breakpoint: 992,
-            settings: {
-                arrow: false,
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                arrow: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-    ],
-});
-$('#logoSectionContent').slick({
-    dots: false,
-    infinite: true,
-    arrow: false,
-    autoplay: true,
-    prevArrow: null,
-    nextArrow: null,
-    speed: 200,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    responsive: [{
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        },
-    ],
-});
+function addSlider() {
+    $('#optionSlider').slick({
+        dots: false,
+        arrow: true,
+        infinite: true,
+        autoplay: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 420,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+        ]
+    });
+    $('#reviewContent').slick({
+        dots: true,
+        infinite: true,
+        arrow: false,
+        prevArrow: null,
+        nextArrow: null,
+        autoplay: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    arrow: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrow: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+        ],
+    });
+    $('#logoSectionContent').slick({
+        dots: false,
+        infinite: true,
+        arrow: false,
+        autoplay: true,
+        prevArrow: null,
+        nextArrow: null,
+        speed: 200,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+        ],
+    });
+}
